b8a403ce498519d80e85c33eba27cfbeebfa9eb4
Matthew Wilson <matthew@productearthsolutions.com>
2016-07-05 21:43:33 +0100

    adding all motor objects

--- motor.py
+++ motor.py
@@ -2,18 +2,18 @@ class Motor:
     def __init__(self, channel, pwm):
         self.channel = channel
         self.pwm = pwm
-        self.speed = 0
+        self.pwmValue = 0
 
     def arm(self):
         print('Arming motor on channel: '+str(self.channel))
-        self.speed = pwmValue
+        self.pwmValue = 260
         self.pwm.set_pwm(self.channel,0,260)
 
     def setSpeed(self, pwmValue):
-        self.speed = pwmValue
+        self.pwmValue = pwmValue
         self.pwm.set_pwm(self.channel,0,pwmValue)
 
     def stop(self):
         print('Stopping motor on channel: '+str(self.channel))
-        self.speed = pwmValue
+        self.speed = 260
         self.pwm.set_pwm(self.channel,0,260)
